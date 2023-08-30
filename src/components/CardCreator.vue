b2086fabacbc9d6e5b017c43cd9bf48329238261
ColluraV <valerio.18.02.89@hotmail.it>
2023-08-30 14:24:20 +0200

    problemi

--- src/components/CardCreator.vue
+++ src/components/CardCreator.vue
@@ -1,15 +1,51 @@
 <script >
-import { store } from '../store'
+import StarRating from 'vue-star-rating'
+import { store, discoverMovies } from '../store'
 export default {
 
     data() {
         return {
-            store,
+            store
         };
     },
-    methods: {
 
-    }
+
+    components:{
+        StarRating,
+    },
+
+    methods: {
+        takeFlags(lang){
+            if(lang == "en"){
+                lang= "gb"}
+                if(lang == "ja"){
+                lang= "jp"}
+                if(lang == "ko"){
+                lang= "kr"}
+                if(lang == "zh"){
+                lang= "cn"}
+                if(lang == "da"){
+                lang= "dk"}
+
+
+        return `https://flagsapi.com/`+ lang.toUpperCase()+`/flat/64.png`  
+        },
+        takeImg(imgs){
+            if(imgs==null){
+                return `https://d994l96tlvogv.cloudfront.net/uploads/film/poster/poster-image-coming-soon-placeholder-no-logo-500-x-740_29376.png`
+            }
+            return `https://image.tmdb.org/t/p/original/`+ imgs
+        },
+        voteConvert(voto){
+                return (voto / 2)
+            },
+        
+        discoverMovies,
+
+    },    
+    mounted(){
+            discoverMovies()
+        },
 
 };
 
@@ -22,14 +58,20 @@ export default {
 
             <div class="card" style="width: 18rem;">
             <img class="card-img-top" alt=""
-            v-bind:src="`https://image.tmdb.org/t/p/original/`+ carta.poster_path">
+            v-bind:src="takeImg(carta.poster_path)">
             <div class="card-body">
                 <h5 class="card-title">{{carta.title}}</h5>
                 <p class="card-text">Original Title: {{carta.original_title}}</p>
                 <p class="card-text">Lingua Originale: {{carta.original_language}} 
                     <img  alt=""
-                    v-bind:src="`https://flagsapi.com/`+ carta.original_language.toUpperCase()+`/flat/64.png`"> </p>
-                <p class="card-text">Voto: {{ carta.vote_average }} / 10</p>
+                    v-bind:src="takeFlags(carta.original_language)"> </p>
+                <star-rating 
+                
+                :increment="0.01" 
+                :fixed-points="2" 
+                :rating="voteConvert(carta.vote_average)"
+                :read-only="true"
+                :star-size="45" ></star-rating>
                 
             </div>
         </div>
--- vite-boolfix/src/store.js
+++ vite-boolfix/src/store.js
@@ -5,12 +5,12 @@ export const store = reactive({
 
     searchtype:"",
     movies:[]
-})
+});
 
 export function filterCards(){
 
-        let url= ``
-https://api.themoviedb.org/3/search/movie?api_key=0e6e1204e5ce59257a879d34cc0f9cb7&query=${store.searchtype}
+        let url= ``;
+        https://api.themoviedb.org/3/search/movie?api_key=0e6e1204e5ce59257a879d34cc0f9cb7&query=${store.searchtype}
         axios.get(url).then((response) => {
 
         store.movies=(response.data.results);
@@ -19,4 +19,15 @@ https://api.themoviedb.org/3/search/movie?api_key=0e6e1204e5ce59257a879d34cc0f9c
     
         }); 
 
-}
+};
+
+export function discoverMovies(){
+        
+        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=0e6e1204e5ce59257a879d34cc0f9cb7&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`).then((response) => {
+
+        store.movies=(response.data.results);
+
+});
+};
+
+
